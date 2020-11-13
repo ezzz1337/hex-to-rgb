c45d17cd7120f8b5ad434d38851ea385c9fd18b4
ezzz <55191289+ezzz1337@users.noreply.github.com>
2020-11-13 19:43:58 +0100

    converter

--- convert.py
+++ convert.py
@@ -2,8 +2,7 @@ def convert(hex_code):
     if len(hex_code.split('#')) != 2 or len(hex_code.split("#")[1]) != 6:
         raise Exception("invalid hex value")
     hex_code = hex_code.lstrip("#")
-    color_tuple = tuple(int(hex_code[x: x+2], 16) for x in (0, 2, 4))
-    return color_tuple
+    return tuple(int(hex_code[x: x+2], 16) for x in (0, 2, 4))
 
 
 if __name__ == "__main__":
