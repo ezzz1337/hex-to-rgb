61b98289b204c841b2faeca678cfef96d63a9544
ezzz <55191289+ezzz1337@users.noreply.github.com>
2020-11-13 01:19:31 +0100

    hex2rgb

--- convert.py
+++ convert.py
@@ -1,6 +1,6 @@
 def convert(hex_code):
     if len(hex_code.split('#')) != 2 or len(hex_code.split("#")[1]) != 6:
-        return
+        raise Exception("invalid hex value")
     hex_code = hex_code.lstrip("#")
     color_tuple = tuple(int(hex_code[x: x+2], 16) for x in (0, 2, 4))
     return color_tuple
